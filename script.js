f86913cf7305f8d60182f6a338c3804ecf86e4d7
bugra-git <237065860+bugra-git@users.noreply.github.com>
2025-11-08 20:33:09 +0300

    Create button functionality and make adjustments to structure

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-const divGrid = document.querySelector("#containerGrid");
-let divRow;
-for (i=1;i<=16;i++) {
-    divRow = document.createElement("div");
-    divRow.classList.add("row");
-    divRow.style.display = "flex";
-    divGrid.appendChild(divRow);
-}
+function makeGrid(num) { 
+    const divGrid = document.querySelector("#containerGrid"); 
+    divGrid.innerHTML = ""; 
 
-const rows = document.querySelectorAll(".row");
-let square;
-rows.forEach((row) => {
-    for (i=1;i<=16;i++) {
-        square = document.createElement("div");
-        square.classList.add("square");
-        square.style.flex = "1";
-        square.style.border = "solid 1px black";
-        row.appendChild(square);
-    }   
-    const computedWidth = getComputedStyle(square).width;
-    row.style.height = computedWidth;
-})
+    const btn = document.createElement("button"); 
+    btn.textContent = "Enter a number for square per row/column"; 
+    btn.style.fontSize = "1em"; 
+    btn.style.fontWeight = "bold"; 
+    btn.style.padding = "1vh"; 
+    divGrid.appendChild(btn); 
+    function btnclick() { 
+        entry = +prompt("maximum:100",""); 
+        if (Number.isInteger(entry)&&entry>0&&entry<=100) { 
+            makeGrid(entry) 
+        } else makeGrid(16); 
+    } 
+    btn.addEventListener("click",btnclick);
 
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-    square.addEventListener("mouseenter",(e)=>{
-        e.target.style.backgroundColor = "black";
-    })
-})
+    let divRow; 
+    for (i=1;i<=num;i++) { divRow = document.createElement("div"); 
+        divRow.classList.add("row"); 
+        divRow.style.display = "flex"; 
+        divGrid.appendChild(divRow); 
+    } 
+    
+    const rows = document.querySelectorAll(".row"); 
+    let square; 
+    rows.forEach((row) => { 
+        for (i=1;i<=num;i++) { 
+            square = document.createElement("div"); 
+            square.classList.add("square"); 
+            square.style.flex = "1"; 
+            square.style.border = "solid 1px black"; 
+            row.appendChild(square); 
+        } 
+    }) 
+    
+    const squares = document.querySelectorAll(".square"); 
+    squares.forEach((square) => { 
+        square.addEventListener("mouseenter",(e)=>{ 
+            e.target.style.backgroundColor = "black"; 
+        }) 
+    }) 
+} 
+
+makeGrid(16);
